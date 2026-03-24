7fa5ea89a8888adf04d1b120e0d2d35a8b023bfe
diogobip <dgbaptista27@gmail.com>
2026-03-24 13:44:32 +0100

    add audio playback and smart download check

--- germanlistening.py
+++ germanlistening.py
@@ -1,6 +1,12 @@
+# IMPORTS
 import yt_dlp
 from pydub import AudioSegment
 import random 
+import pathlib
+from pathlib import Path
+from playsound import playsound
+
+
 
 audio = AudioSegment.from_file("Fabrik-Besuch.webm")
 
@@ -11,7 +17,9 @@ ydl_opts = {
 }
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([URL])
+    file_path = Path("Fabrik-Besuch.webm")
+    if not file_path.exists():
+     ydl.download([URL])
 
 segment_duration = 10 * 1000
 
@@ -20,3 +28,6 @@ random_start = random.randint(0, max_start)
 
 random_segment = audio[random_start : random_start + segment_duration]
 random_segment.export("random_segment.mp3", format="mp3")
+
+playsound('random_segment.mp3')
+
